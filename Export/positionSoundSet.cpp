be7e87219a1ec61693cb70c8ebb966675aec7872
Ted <killian.moine.prive@gmail.com>
2022-07-26 09:14:29 +0200

    Update

---
+++ Export/positionSoundSet.cpp
@@ -0,0 +1,203 @@
+
+    //==========================================
+    //              positionSoundSet
+    //==========================================
+    //==========================================
+    //              Sounds 100M
+    //==========================================
+    class ANIM_Betes_Corbeau1_100M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Betes_Corbeau1_100M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Betes_Corbeau2_100M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Betes_Corbeau2_100M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Betes_Criquets_100M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Betes_Criquets_100M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Creatures_GolemMarcheLourde_100M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Creatures_GolemMarcheLourde_100M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Creatures_Grogne1_100M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Creatures_Grogne1_100M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Creatures_Grogne2_100M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Creatures_Grogne2_100M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Creatures_RespiPreda_100M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Creatures_RespiPreda_100M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Cris_ZombieLoin_100M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Cris_ZombieLoin_100M",
+               1,
+               1,
+               1000
+           };
+    };
+    //==========================================
+    //              Sounds 200M
+    //==========================================
+    class ANIM_Ambi_ForetCorbeaux_200M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Ambi_ForetCorbeaux_200M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Ambi_Grotte1_200M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Ambi_Grotte1_200M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Ambi_Grotte2_200M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Ambi_Grotte2_200M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Ambi_Labo_8min_200M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Ambi_Labo_8min_200M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Ambi_NuitForet_200M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Ambi_NuitForet_200M",
+               1,
+               1,
+               1000
+           };
+    };
+    //==========================================
+    //              Sounds 75M
+    //==========================================
+    class ANIM_Sons_ChuteArbreForet_75M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Sons_ChuteArbreForet_75M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Sons_ChuteBoisMaison_75M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Sons_ChuteBoisMaison_75M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Sons_ChuteBordelMaison_75M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Sons_ChuteBordelMaison_75M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Sons_OsBroyes_75M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Sons_OsBroyes_75M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Sons_PasSurGravier_75M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Sons_PasSurGravier_75M",
+               1,
+               1,
+               1000
+           };
+    };
+    class ANIM_Sons_TocTocToc_75M : default
+    {
+        sound[]=
+           {
+               "\OBF_EA_SoundManager_Sounds\sounds\ANIM_Sons_TocTocToc_75M",
+               1,
+               1,
+               1000
+           };
+    };
--- confgen.py
+++ confgen.py
@@ -1,72 +1,253 @@
-from copy import copy
-from crypt import methods
-from glob import glob
-from http.client import SWITCHING_PROTOCOLS
+from ast import Import
+import copy
 import json
-from mimetypes import init
 import os
 from os.path import exists
-import shutil
-import string
 from datetime import datetime
 
+addon_name = "OBF_EA_SoundManager_Sounds"
 
 def init():
     FileManager.createFolder("Import")
     FileManager.createFolder("Export")
+    SoundManagerGenerator.playerSoundSets()
+    SoundManagerGenerator.positionSoundShader()
+    SoundManagerGenerator.positionSoundSet()
+    SoundManagerGenerator.profile()
     
 
+class SoundManagerGenerator:
+    def playerSoundSets():
+        #VERIFICATION
+        if os.path.exists("Export/playerSoundSets.cpp"):
+            Logger.error("Export/playerSoundSets.cpp already exist!")
+            return
+        
+        Logger.info("Creation of playerSoundSets...")
+        Logger.debug("playerSoundSets file initialisation...")
+        
+        #FILE START
+        FileManager.fileWrite("Export/playerSoundSets.cpp", "    //==========================================")
+        FileManager.fileWrite("Export/playerSoundSets.cpp", "    //              playerSoundSets")
+        FileManager.fileWrite("Export/playerSoundSets.cpp", "    //==========================================")
+        
+        Logger.debug("playerSoundSets file initialised")
+        
+        for folder in os.listdir("Import"):
+            Logger.debug("Folder " + folder + "M")
+            for file in os.listdir("Import/" + folder):
+                Logger.debug("Creation of " + file + " playerSoundSets")
+                
+                FileManager.fileWrite("Export/playerSoundSets.cpp", "    class " + file[:-4] + "_" + folder + "M : default")
+                FileManager.fileWrite("Export/playerSoundSets.cpp", "    {")
+                FileManager.fileWrite("Export/playerSoundSets.cpp", "        sound[]=")
+                FileManager.fileWrite("Export/playerSoundSets.cpp", "           {")
+                FileManager.fileWrite("Export/playerSoundSets.cpp", "               \"\\" + addon_name + "\\sounds\\" + file[:-4] + "_" + folder + "M\",")
+                FileManager.fileWrite("Export/playerSoundSets.cpp", "               1,")
+                FileManager.fileWrite("Export/playerSoundSets.cpp", "               1,")
+                FileManager.fileWrite("Export/playerSoundSets.cpp", "               1000")
+                FileManager.fileWrite("Export/playerSoundSets.cpp", "           };")
+                FileManager.fileWrite("Export/playerSoundSets.cpp", "    };")
+                
+                Logger.debug("Creation of " + file + " playerSoundSets done")
+                
+        Logger.info("Creation of playerSoundSets done")
+    
+        
+        
+        
+    def positionSoundShader():
+        #VERIFICATION
+        if os.path.exists("Export/positionSoundShader.cpp"):
+            Logger.error("Export/positionSoundShader.cpp already exist!")
+            return
+        
+        Logger.info("Creation of positionSoundShader...")
+        Logger.debug("positionSoundShader file initialisation...")
+        
+        #FILE START
+        FileManager.fileWrite("Export/positionSoundShader.cpp", "    //==========================================")
+        FileManager.fileWrite("Export/positionSoundShader.cpp", "    //              positionSoundShader")
+        FileManager.fileWrite("Export/positionSoundShader.cpp", "    //==========================================")
+        
+        Logger.debug("positionSoundShader file initialised")
+        
+        for folder in os.listdir("Import"):
+            Logger.debug("Folder " + folder + "M")
+            #DISTANCE START
+            FileManager.fileWrite("Export/positionSoundShader.cpp", "    //==========================================")
+            FileManager.fileWrite("Export/positionSoundShader.cpp", "    //              Sounds " + folder + "M")
+            FileManager.fileWrite("Export/positionSoundShader.cpp", "    //==========================================")
+            
+            for file in os.listdir("Import/" + folder):
+                Logger.debug("Creation of " + file + " positionSoundShader")
+                
+                FileManager.fileWrite("Export/positionSoundShader.cpp", "    class " + file[:-4] + "_" + folder + "M : default")
+                FileManager.fileWrite("Export/positionSoundShader.cpp", "    {")
+                FileManager.fileWrite("Export/positionSoundShader.cpp", "        sound[]=")
+                FileManager.fileWrite("Export/positionSoundShader.cpp", "           {")
+                FileManager.fileWrite("Export/positionSoundShader.cpp", "               \"\\" + addon_name + "\\sounds\\" + file[:-4] + "_" + folder + "M\",")
+                FileManager.fileWrite("Export/positionSoundShader.cpp", "               1,")
+                FileManager.fileWrite("Export/positionSoundShader.cpp", "               1,")
+                FileManager.fileWrite("Export/positionSoundShader.cpp", "               1000")
+                FileManager.fileWrite("Export/positionSoundShader.cpp", "           };")
+                FileManager.fileWrite("Export/positionSoundShader.cpp", "    };")
+        
+        Logger.info("Creation of positionSoundShader done")
+        
+        
+        
+    def positionSoundSet():
+        #VERIFICATION
+        if os.path.exists("Export/positionSoundSet.cpp"):
+            Logger.error("Export/positionSoundSet.cpp already exist!")
+            return
+        
+        Logger.info("Creation of positionSoundSet...")
+        Logger.debug("positionSoundSet file initialisation...")
+        
+        #FILE START
+        FileManager.fileWrite("Export/positionSoundSet.cpp", "    //==========================================")
+        FileManager.fileWrite("Export/positionSoundSet.cpp", "    //              positionSoundSet")
+        FileManager.fileWrite("Export/positionSoundSet.cpp", "    //==========================================")
+        
+        Logger.debug("positionSoundSet file initialised")
+        
+        for folder in os.listdir("Import"):
+            Logger.debug("Folder " + folder + "M")
+            #DISTANCE START
+            FileManager.fileWrite("Export/positionSoundSet.cpp", "    //==========================================")
+            FileManager.fileWrite("Export/positionSoundSet.cpp", "    //              Sounds " + folder + "M")
+            FileManager.fileWrite("Export/positionSoundSet.cpp", "    //==========================================")
+            
+            for file in os.listdir("Import/" + folder):
+                Logger.debug("Creation of " + file + " positionSoundSet")
+                
+                FileManager.fileWrite("Export/positionSoundSet.cpp", "    class " + file[:-4] + "_" + folder + "M : default")
+                FileManager.fileWrite("Export/positionSoundSet.cpp", "    {")
+                FileManager.fileWrite("Export/positionSoundSet.cpp", "        sound[]=")
+                FileManager.fileWrite("Export/positionSoundSet.cpp", "           {")
+                FileManager.fileWrite("Export/positionSoundSet.cpp", "               \"\\" + addon_name + "\\sounds\\" + file[:-4] + "_" + folder + "M\",")
+                FileManager.fileWrite("Export/positionSoundSet.cpp", "               1,")
+                FileManager.fileWrite("Export/positionSoundSet.cpp", "               1,")
+                FileManager.fileWrite("Export/positionSoundSet.cpp", "               1000")
+                FileManager.fileWrite("Export/positionSoundSet.cpp", "           };")
+                FileManager.fileWrite("Export/positionSoundSet.cpp", "    };")
+        
+        Logger.info("Creation of positionSoundSet done")
+        
+        
+        
+    def profile():
+        if os.path.exists("Export/profil.json"):
+            Logger.error("Export/profil.json already exist!")
+            return
+        
+        Logger.info("Creation of profile file...")
+        SoundsLists = {}
+        Sounds = []
+        
+        for folder in os.listdir("Import"):
+            Logger.debug("Folder " + folder + "M")
+            
+            for file in os.listdir("Import/" + folder):
+                Sounds.append(file[:-4] + "_" + folder + "M")
+
+        SoundsLists["SoundsLists"] = Sounds        
+        FileManager.jsonWrite("Export/profil.json", SoundsLists)
+        Logger.info("Creation of profile file done")
+    
+    
+    
+    def copyFile():
+        return
+
+
 class FileManager:
+    def fileWrite(file, message):
+        fileWrited = open(file, "a")
+        fileWrited.write("\n" + message)
+        fileWrited.close()
+    
+    def jsonWrite(file, array):
+        fileWrited = open(file, "w")
+        json.dump(array, fileWrited, indent=4, sort_keys=True)
+        fileWrited.close()
+        
     def createFolder(folderName):
         if not os.path.exists(folderName):
-            Logger.setInfo(folderName + " folder does not exist")
+            Logger.info(folderName + " folder does not exist")
             try:
                 os.mkdir(folderName)
-                Logger.setInfo(folderName + " folder was created")
+                Logger.info(folderName + " folder was created")
             except:
-                Logger.setError("Unable to create the file")
+                Logger.error("Unable to create the folder")
         else:
-            Logger.setInfo(folderName + " folder exist")
+            Logger.info(folderName + " folder exist")
+    
+    def createFile(fileName):
+        if not exists(fileName):
+            Logger.info(fileName + " does not exist")
+            try:
+                os.mkdir(fileName)
+                Logger.info(fileName + " was created")
+            except:
+                Logger.error("Unable to create the file")
+        else:
+            Logger.info(fileName + " exist")
+        
 
 
 class Logger:
     
-    #0 trace
-    #1 debug
-    #2 info
-    #3 warn
-    #4 error
-    #5 fatal
+    #0 debug
+    #1 info
+    #2 warn
+    #3 error
+    #4 fatal
 
-    logLevel = 2
+    logLevel = 1
 
-    def setTrace(error):
+    def debug(error):
         if Logger.logLevel == 0:
-            print("[TRACE]" , Logger.getDate() , error)
+            message = "[DEBUG] " + Time.getHMS() + " " + error
+            print(message)
+            FileManager.fileWrite(Time.getYMD() + " " + "logs.log", message)
             
-    def setDebug(error):
+    def info(error):
         if Logger.logLevel <= 1:
-            print("[DEBUG]" , Logger.getDate() , error)
-
-    def setInfo(error):
+            message = "[INFO] " + Time.getHMS() + " " + error
+            print(message)
+            FileManager.fileWrite(Time.getYMD() + " " + "logs.log", message)
+    
+    def warn(error):
         if Logger.logLevel <= 2:
-            print("[INFO]" , Logger.getDate() , error)
-    
-    def setWarn(error):
+            message = "[WARN] " + Time.getHMS() + " " + error
+            print(message)
+            FileManager.fileWrite(Time.getYMD() + " " + "logs.log", message)
+            
+    def error(error):
         if Logger.logLevel <= 3:
-            print("[WARN]" , Logger.getDate() , error)
-            
-    def setError(error):
-        if Logger.logLevel <= 4:
-            print("[ERROR]" , Logger.getDate() , error)
+            message = "[ERROR] " + Time.getHMS() + " " + error
+            print(message)
+            FileManager.fileWrite(Time.getYMD() + " " + "logs.log", message)
     
-    def setFatal(error):
-        if Logger.logLevel <= 5:
-            print("[FATAL]" , Logger.getDate() , error)
-            
-    def getDate():
+    def fatal(error):
+        if Logger.logLevel <= 4:
+            message = "[FATAL] " + Time.getHMS() + " " + error
+            print(message)
+            FileManager.fileWrite(Time.getYMD() + " " + "logs.log", message)
+
+
+class Time:
+    def getHMS():
         now = datetime.now()
         date_time_str = now.strftime("%H:%M:%S")
+        return date_time_str
+    
+    def getYMD():
+        now = datetime.now()
+        date_time_str = now.strftime("%Y-%m-%d")
         return date_time_str
 
 init()
